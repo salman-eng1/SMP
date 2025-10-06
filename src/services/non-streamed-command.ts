f2fde259a1600bf3a247ad118cce0b07cd24ffd4
Salman Ali <salman-eng1>
2025-10-06 15:06:33 +0300

    v1.1 final

--- src/services/non-streamed-command.ts
+++ src/services/non-streamed-command.ts
@@ -11,7 +11,7 @@
 //   });
    
 // }
-import { socketIO } from '../server'; // Adjust the import path as needed
+import { socketIO } from '@portal/server'; // Adjust the import path as needed
 const { exec } = require('child_process');
 
 export const execute = (command: string, socketChannel: string): Promise<string> => {
